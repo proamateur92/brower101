49f1c20168b612a07b6ca26f118c2e0a3ed00df3
DongbokLee <68406448+proamateur92@users.noreply.github.com>
2022-02-26 16:46:33 +0900

    Commit: 당근 찾기 게임 js 수정

--- chapter7/main.js
+++ chapter7/main.js
@@ -1,116 +1,190 @@
-// 1. 객체화
-const body = document.querySelector('body');
-const play = document.querySelector('.play');
-const timer = document.querySelector('.timer');
-const counter = document.querySelector('.counter');
-const header = document.querySelector('.header__item');
-const target = document.querySelector('.target');
-let message;
-let stop;
-const bg = new Audio('sound/bg.mp3');
-const alert = new Audio('sound/alert.wav');
-const bugPull = new Audio('sound/bug_pull.mp3');
-const carrotPull = new Audio('sound/carrot_pull.mp3');
-const win = new Audio('sound/game_win.mp3');
+'use strict';
 
-function randomPosition() {
-  target.innerHTML = '';
-  for (let i = 0; i < 10; i++) {
-    const newWidth = Math.floor(Math.random() * 1200 + 1);
-    const newHeight = Math.floor(Math.random() * 230 + 1);
-    target.innerHTML += `<i class='bug' style='transform:translate(${newWidth}px,${newHeight}px)'></i>`;
-  }
+const ITEM_SIZE = 80;
+const CARROT_COUNT = 5;
+const BUG_COUNT = 5;
 
-  for (let i = 0; i < 10; i++) {
-    const newWidth = Math.floor(Math.random() * 1200 + 1);
-    const newHeight = Math.floor(Math.random() * 230 + 1);
-    target.innerHTML += `<i class='carrot' data-id='${i}' style='transform:translate(${newWidth}px,${newHeight}px)'></i>`;
-  }
+let started = false;
+let leftTime = 5;
+let timeOver = undefined;
+let leftCarrot = 0;
 
-  const howMany = document.querySelectorAll('.carrot');
-  counter.innerText = howMany.length;
+const playButton = document.querySelector('.game__play');
+const field = document.querySelector('.game__field');
+const fieldRect = field.getBoundingClientRect();
+const gameTimer = document.querySelector('.game__timer');
+const carrotCounter = document.querySelector('.game__counter');
+const popUp = document.querySelector('.pop-up');
+const replayButton = document.querySelector('.pop-up__replay');
+const popUpMessage = document.querySelector('.pop-up__message');
+const alertSound = new Audio('sound/alert.wav');
+const bgSound = new Audio('sound/bg.mp3');
+const bugSound = new Audio('sound/bug_pull.mp3');
+const carrotSound = new Audio('sound/carrot_pull.mp3');
+const winSound = new Audio('sound/game_win.mp3');
+
+function initGame() {
+  field.innerHTML = '';
+  carrotCounter.innerText = CARROT_COUNT;
+  calcTime(leftTime);
+  addItem('bug', BUG_COUNT, 'img/bug.png');
+  addItem('carrot', CARROT_COUNT, 'img/carrot.png');
 }
 
-function eventCarrot(e) {
-  const id = e.target.dataset.id;
-  const remove = document.querySelector(`.carrot[data-id="${id}"]`);
-  remove.remove();
-  const howMany = document.querySelectorAll('.carrot');
-  counter.innerText = howMany.length;
+function startGame() {
+  leftTime = 5;
+  leftCarrot = 0;
 
-  if (howMany.length === 0) {
-    play.style.transform = `translateY(380px)`;
-    play.innerText = '다시';
-    pop('YOU WON!!');
-    win.play();
+  initGame();
+  showStopButton();
+  showTimerWithCounter();
+  startTimer();
+  bgSound.currentTime = 0;
+  soundPlay(bgSound);
+  started = true;
+}
+
+function stopGame() {
+  stopTimer();
+  hidePlayButton();
+  popUpShow('RETRY?');
+  soundPlay(bugSound);
+  soundPause(bgSound);
+  started = false;
+}
+
+function finishGame(message) {
+  popUpShow(message);
+  hidePlayButton();
+  stopTimer();
+  soundPause(bgSound);
+  started = false;
+}
+
+function addItem(className, count, imgPath) {
+  const x1 = 0;
+  const y1 = 0;
+  const x2 = fieldRect.width - ITEM_SIZE;
+  const y2 = fieldRect.height - ITEM_SIZE;
+
+  for (let i = 0; i < count; i++) {
+    const items = document.createElement('img');
+    items.setAttribute('class', className);
+    items.setAttribute('src', imgPath);
+    items.style.position = 'absolute';
+    let x = fixedLocation(x1, x2);
+    let y = fixedLocation(y1, y2);
+
+    items.style.left = `${x}px`;
+    items.style.top = `${y}px`;
+    field.append(items);
   }
 }
 
-function eventBug() {
-  play.style.transform = `translateY(380px)`;
-  play.innerText = '다시';
-  pop('YOU LOST ㅠㅠ');
+function fixedLocation(min, max) {
+  return Math.floor(Math.random() * (max - min));
 }
 
-function pop(result) {
-  message = document.createElement('div');
-  message.setAttribute('class', 'message');
-  message.innerText = result;
-  header.appendChild(message);
-  clearInterval(stop);
-  if (result !== 'YOU WON!!') {
-    alert.play();
-  }
+function showStopButton() {
+  const icon = document.querySelector('.fas');
+  icon.classList.add('fa-stop');
+  icon.classList.remove('fa-play');
 }
 
-function timeDecrese() {
-  let time = 10;
-  timer.innerText = `00:${time}`;
-  stop = setInterval(() => {
-    timer.innerText = `00:0${--time}`;
-    if (time === 0) {
-      pop('YOU LOST ㅠㅠ');
-      play.style.transform = `translateY(380px)`;
-      play.innerText = '다시';
-      clearInterval(stop);
+function showTimerWithCounter() {
+  gameTimer.style.visibility = 'visible';
+  carrotCounter.style.visibility = 'visible';
+}
+
+function startTimer() {
+  timeOver = setInterval(() => {
+    --leftTime;
+    calcTime(leftTime);
+
+    if (leftTime <= 0) {
+      stopTimer();
+      finishGame('YOU LOST😓');
+      soundPlay(alertSound);
+      return;
     }
   }, 1000);
 }
 
-header.addEventListener('click', e => {
-  if (e.target.className === 'play') {
-    if (play.innerText === '다시') {
-      header.removeChild(message);
-      play.style.transform = `translateY(0px)`;
-      play.innerText = '중단';
-      randomPosition();
-      timeDecrese();
-    } else if (play.innerText === '중단') {
-      play.style.transform = `translateY(380px)`;
-      play.innerText = '다시';
-      pop('YOU LOST ㅠㅠ');
-    }
+function calcTime(time) {
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  gameTimer.innerHTML = `${minutes}:${seconds}`;
+}
 
-    if (play.innerText === '클릭') {
-      bg.play();
-      randomPosition();
-      play.innerText = '중단';
-      timeDecrese();
-    }
-  }
-});
+function hidePlayButton() {
+  playButton.style.visibility = 'hidden';
+}
 
-target.addEventListener('click', e => {
-  if (e.target.className === 'carrot') {
-    carrotPull.play();
-    eventCarrot(e);
+function pickItem(event) {
+  if (!started) {
     return;
   }
 
-  if (e.target.className === 'bug') {
-    bugPull.play();
-    clearInterval(stop);
-    eventBug();
-    return;
+  let target = event.target;
+
+  if (target.matches('.carrot')) {
+    field.removeChild(target);
+    leftCarrot++;
+    countCarrot(leftCarrot);
+    carrotSound.currentTime = 0;
+    soundPlay(carrotSound);
+
+    if (CARROT_COUNT === leftCarrot) {
+      finishGame('😆YOU WIN💯');
+    }
+  }
+
+  if (target.matches('.bug')) {
+    finishGame('😓YOU LOST');
+  }
+}
+
+function countCarrot(leftCarrot) {
+  carrotCounter.innerText = CARROT_COUNT - leftCarrot;
+}
+
+function stopTimer() {
+  clearInterval(timeOver);
+}
+
+function popUpShow(text) {
+  popUpMessage.innerText = text;
+  popUp.classList.remove('pop-up__hide');
+}
+
+function hidePopUp() {
+  popUp.classList.add('pop-up__hide');
+}
+
+function showPlayButton() {
+  playButton.style.visibility = 'visible';
+}
+
+function soundPlay(sound) {
+  sound.play();
+}
+
+function soundPause(sound) {
+  sound.pause();
+}
+
+playButton.addEventListener('click', () => {
+  if (!started) {
+    startGame();
+  } else {
+    stopGame();
   }
 });
+
+replayButton.addEventListener('click', () => {
+  hidePopUp();
+  showPlayButton();
+  startGame();
+});
+
+field.addEventListener('click', pickItem);
